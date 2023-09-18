4c9d0d1e268e1d5251a456c333f94b3de7b79c99
PepeTonin <phavilat@hotmail.com>
2023-09-18 14:55:13 -0300

    fix: fix email input to always transform to lower case before store

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -7,7 +7,7 @@ const API_KEY = 'AIzaSyBPpwE5idc2__CFguEwmWdSlJRISRWR_bo';
 async function authenticate(mode: string, email: string, password: string) {
   const url = `${AUTH_ENDPOINT}${mode}?key=${API_KEY}`;
   const response = await axios.post(url, {
-    email: email,
+    email: email.toLowerCase(),
     password: password,
     returnSecureToken: true,
   });
@@ -26,5 +26,5 @@ export function login(email: string, password: string) {
 
 export async function resetPassword(email: string) {
   const url = `${AUTH_ENDPOINT}sendOobCode?key=${API_KEY}`;
-  await axios.post(url, { requestType: 'PASSWORD_RESET', email: email });
+  await axios.post(url, { requestType: 'PASSWORD_RESET', email: email.toLowerCase() });
 }
